fd22ff94997f56ab27758fd0b5d4c752a01643e0
jaeee <wldms1107@gmail.com>
2022-08-09 16:22:46 +0900

    복구.....

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Main from "./pages/Main";
-import ProductListPage from "./pages/ProductListPage";
-import Cart from "./pages/Cart";
-import ProductPage from "./pages/ProductPage";
-import NotFound from "./pages/NotFound";
-import Result from "./pages/Result";
+import { Route, Routes } from "react-router-dom";
+import Todo from "./pages/Todo";
+import SbReact from "./pages/SbReact";
+import SeoulBund from "./pages/Seoulbund";
+import Pot from "./pages/Pot";
+import Lib from "./pages/Lib";
 
 function App() {
   return (
-    <>
-      <Header />
+    <div>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/cups" element={<ProductListPage />}></Route>
-        <Route path="/cups/:title" element={<ProductPage loca={"cups"} />} />
-        <Route path="/tableware" element={<ProductListPage />}></Route>
-        <Route
-          path="/tableware/:title"
-          element={<ProductPage loca={"tableware"} />}
-        />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/search" element={<Result />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/todolist" element={<Todo />} />
+        <Route path="/pot" element={<Pot />} />
+        <Route path="/changwonlib" element={<Lib />} />
+        <Route path="/seoulbundreact" element={<SbReact />} />
+        <Route path="/seoulbund" element={<SeoulBund />} />
       </Routes>
-
-      <Footer />
-    </>
+    </div>
   );
 }
 
